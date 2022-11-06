a24b59add847103034574ae9186786a401198d64
YongxuePeng <yongxue.peng@foodpanda.com>
2022-11-06 18:50:09 +0800

    resolve issue with payment transaction

--- wholesale-ycql/src/main/java/edu/cs5424/transactions/PaymentTransaction.java
+++ wholesale-ycql/src/main/java/edu/cs5424/transactions/PaymentTransaction.java
@@ -5,15 +5,19 @@ import com.datastax.oss.driver.api.core.cql.*;
 
 import java.math.BigDecimal;
 
-public class PaymentTransaction extends BaseTransaction{
+public class PaymentTransaction extends BaseTransaction {
     private final int w_id;
     private final int d_id;
     private final int c_id;
     private final BigDecimal payment;
+    private BigDecimal w_ytd_amount;
+    private BigDecimal d_ytd_amount;
+    private float c_ytd_amount;
+    private int c_payment_cnt;
     private String w_street_1, w_street_2, w_city, w_state, w_zip;
     private String d_street_1, d_street_2, d_city, d_state, d_zip;
     private String c_first, c_middle, c_last, c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_since, c_credit;
-    private double c_credit_lim, c_discount, c_balance;
+    private BigDecimal c_credit_lim, c_discount, c_balance;
     private CqlSession session = null;
 
     public PaymentTransaction(CqlSession session, String[] params) {
@@ -59,7 +63,7 @@ public class PaymentTransaction extends BaseTransaction{
 
     private void queryWarehouse() {
         String query = String.format("""
-                SELECT W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP
+                SELECT W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP, W_YTD
                 FROM Warehouse
                 WHERE W_ID = %d
                 """, w_id);
@@ -71,21 +75,23 @@ public class PaymentTransaction extends BaseTransaction{
             w_city = row.getString("W_CITY");
             w_state = row.getString("W_STATE");
             w_zip = row.getString("W_ZIP");
+            w_ytd_amount = row.getBigDecimal("W_YTD");
         }
     }
 
     private void updateWarehouse() {
+        BigDecimal amt = w_ytd_amount.add(payment);
         String query = String.format("""
                 UPDATE Warehouse
-                SET W_YTD = W_YTD + %.2f
+                SET W_YTD = %.2f
                 WHERE W_ID = %d
-                """, payment, w_id);
+                """, amt, w_id);
         this.session.execute(query);
     }
 
     private void queryDistrict() {
         String query = String.format("""
-                SELECT D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP
+                SELECT D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP, D_YTD
                 FROM District
                 WHERE D_W_ID = %d AND D_ID = %d
                 """, w_id, d_id);
@@ -97,21 +103,23 @@ public class PaymentTransaction extends BaseTransaction{
             d_city = row.getString("D_CITY");
             d_state = row.getString("D_STATE");
             d_zip = row.getString("D_ZIP");
+            d_ytd_amount = row.getBigDecimal("D_YTD");
         }
     }
 
     private void updateDistrict() {
+        BigDecimal amt = d_ytd_amount.add(payment);
         String query = String.format("""
                 UPDATE District
-                SET D_YTD = D_YTD + %.2f
+                SET D_YTD = %.2f
                 WHERE D_W_ID = %d AND D_ID = %d
-                """, payment, w_id, d_id);
+                """, amt, w_id, d_id);
         this.session.execute(query);
     }
 
     private void queryCustomer() {
         String query = String.format("""
-                SELECT C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE
+                SELECT C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT
                 FROM Customer
                 WHERE C_W_ID = %d AND C_D_ID = %d AND C_ID = %d
                 """, w_id, d_id, c_id);
@@ -127,20 +135,27 @@ public class PaymentTransaction extends BaseTransaction{
             c_state = row.getString("C_STATE");
             c_zip = row.getString("C_ZIP");
             c_phone = row.getString("C_PHONE");
-            c_since = row.getString("C_SINCE");
+            c_since = row.getInstant("C_SINCE").toString();
             c_credit = row.getString("C_CREDIT");
-            c_credit_lim = row.getDouble("C_CREDIT_LIM");
-            c_discount = row.getDouble("C_DISCOUNT");
-            c_balance = row.getDouble("C_BALANCE");
+            c_credit_lim = row.getBigDecimal("C_CREDIT_LIM");
+            c_discount = row.getBigDecimal("C_DISCOUNT");
+            c_balance = row.getBigDecimal("C_BALANCE");
+            c_ytd_amount = row.getFloat("C_YTD_PAYMENT");
+            c_payment_cnt = row.getInt("C_PAYMENT_CNT");
         }
     }
 
     private void updateCustomer() {
+        BigDecimal balance = c_balance.subtract(payment);
+        BigDecimal ytd_amt = new BigDecimal(c_ytd_amount);
+        BigDecimal amt = ytd_amt.add(payment);
+        int paymentCNT = c_payment_cnt + 1;
+        
         String query = String.format("""
                 UPDATE Customer
-                SET C_BALANCE = C_BALANCE - %f, C_YTD_PAYMENT = C_YTD_PAYMENT + %f, C_PAYMENT_CNT = C_PAYMENT_CNT + 1
+                SET C_BALANCE = %.2f, C_YTD_PAYMENT = %.2f, C_PAYMENT_CNT = %d
                 WHERE C_W_ID = %d AND C_D_ID = %d AND C_ID = %d
-                """, payment, payment, w_id, d_id, c_id);
+                """, balance, amt, paymentCNT, w_id, d_id, c_id);
         this.session.execute(query);
     }
 }
